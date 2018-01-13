d6ce80bea094851cbcd4a2d6ea5fe670a7ed574d
Spencer Hamm <spentacular@gmail.com>
2018-01-12 23:08:36 -0600

    remove command duration length

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -8,13 +8,6 @@ function fish_right_prompt
   set -l status_copy $status
   set -l dir (basename (prompt_pwd))
 
-  if test $CMD_DURATION -gt 500
-    set -l duration (echo $CMD_DURATION | humanize_duration)
-    set_color B56FF6
-    echo -n "$duration "
-    set_color normal
-  end
-
   set_color $sh_color1
   echo -n "$dir"
   set_color normal
