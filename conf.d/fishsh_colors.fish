a70da6aecd40e571f2ef2bc728a1dcbf56de610f
Spencer Hamm <spentacular@gmail.com>
2019-01-07 10:02:42 -0600

    update git styles

--- conf.d/fishsh_colors.fish
+++ conf.d/fishsh_colors.fish
@@ -8,16 +8,18 @@ set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_showupstream 'yes'
 
-set __fish_git_prompt_color_dirtystate E84A5F
-set __fish_git_prompt_color_untrackedfiles FCE38A
-set __fish_git_prompt_color_upstream 72FBF4
-set __fish_git_prompt_color_stagedstate 8DE4DC
+set __fish_git_prompt_color_dirtystate red
+set __fish_git_prompt_color_untrackedfiles yellow
+set __fish_git_prompt_color_upstream cyan
+set __fish_git_prompt_color_stagedstate blue
+set __fish_git_prompt_color_stashstate magenta
 
 set __fish_git_prompt_char_dirtystate '•'
 set __fish_git_prompt_char_stagedstate '•'
-set __fish_git_prompt_char_untrackedfiles '•'
+set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_stashstate '↩'
 set __fish_git_prompt_char_upstream_equal ''
 set __fish_git_prompt_char_upstream_prefix ''
-set __fish_git_prompt_char_upstream_ahead ' ⇡'
-set __fish_git_prompt_char_upstream_behind ' ⇣'
+set __fish_git_prompt_char_upstream_ahead ' ↑'
+set __fish_git_prompt_char_upstream_behind ' ↓'
 set __fish_git_prompt_char_upstream_diverged ' ↔'
